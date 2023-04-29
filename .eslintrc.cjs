f100f5236917636c825b67d84a52dcfe7ff25901
Leonardo Copello <copello@mobtex.com.br>
2023-04-29 09:34:53 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+// module.exports = {
+//   root: true,
+//   extends: '@react-native-community',
+// };
+
+/* eslint-env node */
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from "react-native";
+
+export const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Teste</Text>
+    </SafeAreaView>
+  );
+}
